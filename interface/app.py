2c94af30b439a9e65b6a18cb3dad8800d2d0359b
heeve <nazarpasternak2001@gmail.com>
2020-05-19 16:13:29 +0300

    tweak

--- interface/app.py
+++ interface/app.py
@@ -36,6 +36,7 @@ def submit_check():
     for box, text in zip(boxes_status, check_text):
         if box:
             chosen_bands.append(text)
+    reroll_bands()
 
 
 def submit_radio():
@@ -59,8 +60,8 @@ def submit_radio():
 
         radio_text = get_next_songs(curr_band_songs)
         create_radio_buttons(radio_text)
-        lbl = Label(window, text="Choose one song of {}".format(chosen_bands[curr_band_index]))
-        lbl.place(x=200, y=100)
+        lbl = Label(window, text="Choose one song of {}".format(chosen_bands[curr_band_index]), font=BIG_FONT)
+        lbl.place(x=450, y=150)
     else:
         show_results()
 
@@ -152,7 +153,7 @@ def reroll_bands():
 
 
 def go_to_songs():
-    global curr_band_songs, radio_text, submit_btn, reroll_songs_btn
+    global curr_band_songs, radio_text, submit_btn, reroll_songs_btn, lbl
 
     if len(chosen_bands) == 0:
         messagebox.showinfo("Oops...", "You haven't chosen any band")
@@ -164,7 +165,6 @@ def go_to_songs():
     radio_text = get_next_songs(curr_band_songs)
 
     create_radio_buttons(radio_text)
-
     lbl = Label(window, text="Choose one song of {}".format(chosen_bands[curr_band_index]), font=BIG_FONT)
     lbl.place(x=450, y=150)
 
@@ -172,7 +172,7 @@ def go_to_songs():
     submit_btn.place(x=600, y=350)
 
     reroll_songs_btn = Button(window, text="Rerrol songs", command=reroll_songs)
-    reroll_songs_btn.place(x=900, y=150)# reroll_songs_btn = Button(window, text="Rerrol songs", command=reroll_songs)
+    reroll_songs_btn.place(x=700, y=350)# reroll_songs_btn = Button(window, text="Rerrol songs", command=reroll_songs)
 
 
     # next_band_btn = Button(window, text="Next band", command=next_band)
@@ -214,7 +214,7 @@ def destroy_radio_buttons():
 
 
 def create_radio_buttons(text_lst):
-    x = 150
+    x = 300
     y = 250
     value = 0
     for i in range(len(text_lst)):
@@ -222,7 +222,7 @@ def create_radio_buttons(text_lst):
 
         rb = Radiobutton(window, text=curr_text, variable=radio_var, value=value)
         if i == len(text_lst) / 2:
-            x = 150
+            x = 300
             y = 300
         rb.place(x=x, y=y)
         radio_buttons.append(rb)
@@ -232,7 +232,7 @@ def create_radio_buttons(text_lst):
 
 
 def create_check_buttons(text_lst):
-    x = 150
+    x = 250
     for i in range(len(text_lst)):
         var = BooleanVar()
         var.set(False)
@@ -250,17 +250,17 @@ check_text = get_next_bands()
 create_check_buttons(check_text)
 
 lbl = Label(window, text="Choose bands/artist that you listen to (up to 3):", font=BIG_FONT)
-lbl.place(x=450, y=150)
+lbl.place(x=350, y=150)
 
 submit_btn = Button(window, text="Submit", command=submit_check)
-submit_btn.place(x=600, y=350)
+submit_btn.place(x=450, y=350)
 
 
 reroll_btn = Button(window, text='Reroll bands', command=reroll_bands)
-reroll_btn.place(x=900, y=150)
+reroll_btn.place(x=550, y=350)
 
 go_to_songs_btn = Button(window, text='Choose songs', command=go_to_songs)
-go_to_songs_btn.place(x=900, y=350)
+go_to_songs_btn.place(x=680, y=350)
 
 
 window.mainloop()
